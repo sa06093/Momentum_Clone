f5cdee571562fef20a7131f4819dad85608941f0
Seungyeop Lee <45029511+sa06093@users.noreply.github.com>
2024-01-07 17:33:39 +0900

    Update placeholder.js
    restorePlaceholder 함수 하나로 통합

--- js/placeholder.js
+++ js/placeholder.js
@@ -1,24 +1,19 @@
+let = originalPlaceholders = {};
+
 // 마우스 클릭되었을 시 placeholder 제거
 function hidePlaceholder(element) {
+    const inputID = element.id;
+    originalPlaceholders[inputID] = element.placeholder;
     element.removeAttribute("placeholder");
-    }
+}
 
     
 // 마우스 클릭 취소되었을 시, placeholder 다시 생성
-function restorePlaceholderTodo(element) {
+function restorePlaceholder(element) {
+    const inputID = element.id;
     if (element.value === "") {
-        element.setAttribute("placeholder", "Write a to Do");
-    }
-}
-
-function restorePlaceholderName(element) {
-    if (element.value === "") {
-        element.setAttribute("placeholder", "What is your name?");
-    }
-}
-
-function restorePlaceholderSearch(element) {
-    if (element.value === "") {
-        element.setAttribute("placeholder", "Type your search term");
+        const originalPlaceholder = originalPlaceholders[inputID];
+        console.log(originalPlaceholder);
+        element.setAttribute("placeholder", originalPlaceholder);
     }
 }
